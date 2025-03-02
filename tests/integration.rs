3ccd1e6a797b3e2dc0f0d8746b264f22836e1eb4
Jacob Sjöström <jacsjs-dev@pm.me>
2025-03-03 00:56:28 +0100

    Prepare for test integration

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,7 +1,5 @@
 #![no_std]
 #![no_main]
-pub mod mono;
-pub mod at_command_handler;
 
 #[cfg(test)]
 use defmt_rtt as _; // global logger
@@ -12,12 +10,10 @@ use nrf52840_hal as _; // memory layout
 
 use panic_halt as _;
 
-// defmt-test 0.3.0 has the limitation that this `#[tests]` attribute can only be used
-// once within a crate. the module can be in any file but there can only be at most
-// one `#[tests]` module in this library crate
-#[cfg(test)]
+// See https://crates.io/crates/defmt-test/0.3.0 for more documentation (e.g. about the 'state'
+// feature)
 #[defmt_test::tests]
-mod unit_tests {
+mod tests {
     use defmt::assert;
 
     #[test]
